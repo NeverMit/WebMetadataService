b1a60db0edfb535e701d60da4eec1b374020edea
NeverMit <markovnikov96@mail.ru>
2024-03-13 15:56:13 +0500

    Разделил FileScanner на два сервиса. Проблема с GetFiles() осталась

---
+++ WebMetadataService/MetadataService/FileHandlerService.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace WebMetadataService.MetadataService;
+//класс для работы с файлом
+public class FileHandlerService:BackgroundService
+{
+    private readonly FileMetadataProvider _fileMetadataProvider;
+    private readonly FileScannerOptions _options;
+    public FileHandlerService(FileMetadataProvider fileMetadataProvider,IOptions<FileScannerOptions> options)
+    {
+        _fileMetadataProvider = fileMetadataProvider;
+        _options = options.Value;
+    }
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            try
+            {
+                if (_options.Path != null && _options.SearchPattern != null)
+                {
+                    await GetSearchedFilesAsync(stoppingToken);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            await Task.Delay(TimeSpan.FromSeconds(_options.Interval), stoppingToken);
+        }
+    }
+    private async Task GetSearchedFilesAsync(CancellationToken stoppingToken)
+    {
+        var files = Directory.GetFiles
+        (
+            _options.Path,
+            _options.SearchPattern
+        );
+        Console.WriteLine($"Найдено {files.Length} файлов");
+        foreach (var file in files)
+        {
+            var fileData = await _fileMetadataProvider.ReadFileDataAsync(file);
+            _fileMetadataProvider.FindFileChanges(fileData);
+            if (_fileMetadataProvider.Files == null)
+            {
+                Console.WriteLine("Files пустой");
+            }
+            else
+            {
+                Console.WriteLine
+                    (
+                        $"Количество обрабатываемых файлов: {_fileMetadataProvider.Files.Count}"
+                    );
+            }
+            Console.WriteLine
+            (
+                $"File:{fileData.FileName}," +
+                $"Hash:{fileData.FileHash}"
+            );
+        }
+    }
+}
---
+++ WebMetadataService/MetadataService/FileMetadataProvider.cs
@@ -0,0 +1,55 @@
+﻿using System.Security.Cryptography;
+using Microsoft.Extensions.Options;
+
+namespace WebMetadataService.MetadataService;
+// класс для десериализации, валидации, хранения данных файлов и сравнения хэшей. 
+public class FileMetadataProvider
+{
+    public List<FileData> Files { get; set; } = new();
+    
+    public void FindFileChanges(FileData fileData)
+    {
+        
+        var f = Files.Find
+        (
+            x =>
+                x.FileName.Equals
+                (
+                    fileData.FileName,
+                    StringComparison.CurrentCultureIgnoreCase
+                )
+        );           
+        if (f == null)
+        {
+            Files.Add(fileData);
+        }
+        else
+        {
+            if (f.FileHash != fileData.GetHashCode())
+            {
+                Console.WriteLine(f.FileName + " изменен");
+                f.FileHash = fileData.GetHashCode();
+            }
+        }
+    }
+    //чтение содержимого файла
+    public  async Task<FileData> ReadFileDataAsync(string filePath)
+    {
+        await using var fileStream = new FileStream
+        (
+            filePath,
+            FileMode.Open,
+            FileAccess.Read
+        );
+        using var streamReader = new StreamReader(fileStream);
+        var fileContent = await streamReader.ReadToEndAsync();
+        var fileName = Path.GetFileName(filePath);
+        var fileData = new FileData
+        {
+            FileName = fileName,
+            FileHash = fileContent.GetHashCode(),
+            FileContent=fileContent
+        };
+        return fileData;
+    }
+}
